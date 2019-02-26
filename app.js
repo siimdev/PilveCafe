c58a59a11bd270b013d3833aa4a76ad30fe8ec4b
Siim <siim.pwn@gmail.com>
2019-02-26 23:11:51 +0200

    responsive

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA65egZU9kkkA3yu9B_wfJBGoO1wU7sfyU",
+    authDomain: "pilve-cafe.firebaseapp.com",
+    databaseURL: "https://pilve-cafe.firebaseio.com",
+    projectId: "pilve-cafe",
+    storageBucket: "pilve-cafe.appspot.com",
+    messagingSenderId: "627852924046"
+};
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
+
 const cafeList = document.querySelector('#cafe-list');
 const form = document.querySelector('#add-cafe-form');
 
 // create element and render cafe
-function renderCafe(doc){
+function renderCafe(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
@@ -28,12 +41,14 @@ function renderCafe(doc){
 }
 
 // getting data
-db.collection('cafes').where('city', '==', 'Tallinn').orderBy('name').get().then((snapshot) => {
+// db.collection('cafes').where('city', '==', 'Tallinn').orderBy('name').get().then((snapshot) => {
 // db.collection('cafes').orderBy('city').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-    })
-})
+
+// db.collection('cafes').get().then((snapshot) => {
+//     snapshot.docs.forEach(doc => {
+//         renderCafe(doc);
+//     })
+// })
 
 // saving data
 form.addEventListener('submit', (e) => {
@@ -44,4 +59,18 @@ form.addEventListener('submit', (e) => {
     })
     form.name.value = '';
     form.city.value = '';
+})
+
+
+// Real time listener
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if (change.type == 'added') {
+            renderCafe(change.doc);
+        } else if (change.type == 'removed') {
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+    })
 })
